fc554531dc710d126d3abb0e61c15de848f241b7
Alexey7272 <106250222+Alexey7272@users.noreply.github.com>
2022-11-12 16:03:47 +0200

    add start file

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './css/styles.css';
+
+const DEBOUNCE_DELAY = 300;
+
+// function fetchCountries(name) {
+//     fetch(`https://restcountries.com/v3.1/name/${name}`)
+//     .then(response => response.json())
+//     .then(data => console.log('name', name));
+// };
+
+// fetchCountries('Ukraine');
